e891b588d3a4a3b78eba68b094c68bad5cb42e98
BaumTheFeljoy <baumthefeljoy@live.de>
2019-12-06 15:41:06 +0100

    adjust winston formatting & exit on internal error

--- server/routes/features.js
+++ server/routes/features.js
@@ -4,16 +4,6 @@ const mongoose = require("mongoose")
 const { Project } = require("../models/project")
 const { validateFeature } = require("../models/feature")
 
-/*// TODO: not sure if this is even needed
-router.get("/:projectId", async (req, res) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.projectId)) return res.status(400).send("ProjectId doesn't fit id schema")
-
-    const project = await Project.findById(req.params.projectId)
-    if (!project) return res.status(404).send("Invalid projectId")
-
-    res.send(project.features)
-});*/
-
 router.post("/:projectId", async (req, res) => {
     const { error } = validateFeature(req.body)
     if (error) return res.status(400).send(error.details[0].message)
@@ -29,7 +19,7 @@ router.post("/:projectId", async (req, res) => {
         description: req.body.description,
         employeeIds: [],
         userIds: [],
-        creator: req.body.creatorId
+        creator: req.cookies["userId"]
     })
     await project.save()
 
