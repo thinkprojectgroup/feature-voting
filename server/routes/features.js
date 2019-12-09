12994fa7a870df57c281af7a77893de30cd261b9
Fellix Lausch <baumthefeljoy@live.de>
2019-12-09 17:42:14 +0100

    fix unsuccesful merge

--- server/routes/features.js
+++ server/routes/features.js
@@ -13,7 +13,7 @@ router.post("/:projectId", uploadImages, async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.params.projectId)) return res.status(400).send("ProjectId doesn't fit id schema")
 
     // Using find & save instead of update for featureSchema.pre method to work properly
-    const project = await Project.findOne({_id: req.params.projectId, deleted: false})
+    const project = await Project.findOne({ _id: req.params.projectId, deleted: false })
     if (!project) return res.status(404).send("Invalid projectId")
 
     project.features.push({
@@ -29,8 +29,6 @@ router.post("/:projectId", uploadImages, async (req, res) => {
     res.status(201).send(project)
 })
 
-<<<<<<< HEAD
-=======
 // Patch request to set acceptedStatus=true for given featureId
 router.patch("/accept/:featureId", async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.params.featureId)) return res.status(400).send("FeatureId doesn't fit id schema")
@@ -40,7 +38,7 @@ router.patch("/accept/:featureId", async (req, res) => {
             { "features._id": req.params.featureId },
             { "$set": { "features.$.acceptedStatus": true } })
 
-    if(result.nModified != 1) return res.status(404).send("FeatureId not found (or acceptedStatus was already true)")
+    if (result.nModified != 1) return res.status(404).send("FeatureId not found (or acceptedStatus was already true)")
 
     res.status(204).send()
 })
@@ -72,54 +70,29 @@ router.patch("/vote/:featureId", async (req, res) => {
 })
 
 // Get specific feature for project & feature id
->>>>>>> dev
 router.get("/:projectId/:featureId", async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.params.projectId)) return res.status(400).send("ProjectId doesn't fit id schema")
     if (!mongoose.Types.ObjectId.isValid(req.params.featureId)) return res.status(400).send("FeatureId doesn't fit id schema")
 
-<<<<<<< HEAD
-    const project = await Project.findById(req.params.projectId)
-=======
-    const project = await Project.findOne({_id: req.params.projectId, deleted: false})
->>>>>>> dev
+    const project = await Project.findOne({ _id: req.params.projectId, deleted: false })
     if (!project) return res.status(404).send("projectId not found")
 
     //TODO add featureID invalid response
 
     const feature = project.features.id(req.params.featureId)
-<<<<<<< HEAD
-    if (!feature) return res.status(404).send("featureId not found")
-=======
     if (!feature || feature.deleted) return res.status(404).send("featureId not found")
->>>>>>> dev
 
     res.send(feature)
 });
 
-<<<<<<< HEAD
-=======
 // Delete specific feature for project & feature id
->>>>>>> dev
 router.delete("/:projectId/:featureId", async (req, res) => {
 
     if (!mongoose.Types.ObjectId.isValid(req.params.projectId)) return res.status(400).send("ProjectId doesn't fit id schema")
     if (!mongoose.Types.ObjectId.isValid(req.params.featureId)) return res.status(400).send("FeatureId doesn't fit id schema")
 
     // Using find & save instead of update for featureSchema.pre method to work properly
-<<<<<<< HEAD
-    const project = await Project.findById(req.params.projectId)
-    if (!project) return res.status(404).send("Invalid projectId")
-
-    const feature = project.features.id(req.params.featureId)
-    if (!feature) return res.status(404).send("featureId not found")
-
-    project.features.pull(req.params.featureId)
-
-    await project.save()
-
-    res.status(201).send("successfully deleted feature")
-=======
-    const project = await Project.findOne({_id: req.params.projectId, deleted: false})
+    const project = await Project.findOne({ _id: req.params.projectId, deleted: false })
     if (!project) return res.status(404).send("Invalid projectId")
 
     const feature = await project.features.id(req.params.featureId)
@@ -127,7 +100,7 @@ router.delete("/:projectId/:featureId", async (req, res) => {
 
     const result = await Project.findOneAndUpdate(
         { "_id": req.params.projectId, "features._id": req.params.featureId },
-        { 
+        {
             "$set": {
                 "features.$.deleted": true
             }
@@ -137,7 +110,6 @@ router.delete("/:projectId/:featureId", async (req, res) => {
     await project.save()
 
     res.status(202).send(result)
->>>>>>> dev
 })
 
 module.exports = router;
