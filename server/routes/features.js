668eecf3b7bac82617568437208f24957fff69ff
BaumTheFeljoy <baumthefeljoy@live.de>
2019-12-13 15:07:40 +0100

    add comment to post feature req

--- server/routes/features.js
+++ server/routes/features.js
@@ -5,7 +5,8 @@ const { Project } = require("../models/project")
 const { validateFeature } = require("../models/feature")
 const uploadImages = require("../middleware/imageUpload")
 
-
+// Post new feature to given projectId. Request needs to be in form-data otherwise
+// upload middleware will throw an error.
 router.post("/:projectId", uploadImages, async (req, res) => {
     const { error } = validateFeature(req.body)
     if (error) return res.status(400).send(error.details[0].message)
