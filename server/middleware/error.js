25b2c025bd5f5a357f78ba21841d9caa3eac64c8
BaumTheFeljoy <35038719+BaumTheFeljoy@users.noreply.github.com>
2019-12-17 00:46:31 +0100

    Add text indexing (#124)
    * add text indexes & unique project names
    
    * add comment to post feature req
    
    * add comments to requests, finalize indexing
    
    * add validation for search requests
    
    * small adjustments & fix double error logging
    
    * fix double error logging, add error-log file
    
    * CLIENTadd Login Component, SERVER added auth model/middleware/route

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -3,7 +3,7 @@ const winston = require("winston")
 // Used as the last middleware function in routes.js this catches all errors thrown by 
 // other middleware functions (mainly db connection issues)
 module.exports = function(err, req, res, next){
-    winston.error(err.message, err)
-    res.status(500).send("Something failed")
+    winston.error(`Role: ${res.role} ${err.message}`)
+    res.status(500).send(err.message)
     process.exit()
 }
--- server/startup/logging.js
+++ server/startup/logging.js
@@ -2,7 +2,7 @@ const winston = require("winston")
 
 require("express-async-errors") // Automatically catches express async errors and passes control to error middleware function
 
-const format = winston.format.combine(
+const consoleFormat = winston.format.combine(
     winston.format.colorize(),
     winston.format.timestamp(),
     winston.format.align(),
@@ -11,15 +11,17 @@ const format = winston.format.combine(
     })
 )
 
+const format = winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.align(),
+    winston.format.printf(({ level, message, label, timestamp }) => {
+        return `${timestamp} ${level}: ${message}`;
+    })
+)
+
 module.exports = function () {
-    // Winston setup
-    winston.add(winston.createLogger({
-        transports: [new winston.transports.Console({
-            format: format,
-            handleExceptions: true
-        })],
-        exitOnError: true
-    }))
+    winston.add(new winston.transports.Console({ level: "info", handleExceptions: true, format: consoleFormat }))
+    winston.add(new winston.transports.File({ filename: "error-log.txt", level: "error", handleExceptions: true, format: format }))
 
     process.on("unhandledRejection", (ex) => { throw ex }) // Catch UnhandledPromiseRejections and rethrow as normal exceptions
 }
