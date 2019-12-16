2b8e3e159b57189fbd7901167db7291002c016d3
Sebastian-Zoske <s0554384@htw-berlin.de>
2019-12-16 15:04:08 +0100

    Update error.js

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -3,7 +3,7 @@ const winston = require("winston")
 // Used as the last middleware function in routes.js this catches all errors thrown by 
 // other middleware functions (mainly db connection issues)
 module.exports = function(err, req, res, next){
-    console.log("error.js", res.role);
     winston.error(err.message, err)
+    res.status(500).send("Something failed")
     process.exit()
 }
