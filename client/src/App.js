f6da90e20ee5e93c769e7c7f9b100ecca79f4f46
EmaStoyanova <emaestoyanova@gmail.com>
2019-12-17 22:56:17 +0100

    project overview added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import FeatureDetailView from './components/FeatureDetailView';
-import ProjectDetailView from './components/ProjectDetailView';
-import Header from './components/Header'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import React, { Component } from "react";
+import "./App.css";
+import FeatureDetailView from "./components/FeatureDetailView";
+import ProjectDetailView from "./components/ProjectDetailView";
+import Header from "./components/Header";
+import ProjectOverView from "./components/ProjectOverView";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
-
-
   render() {
     return (
-
-
       <Router>
-        <Header/>
-        <Route path={"/"} exact component={ProjectDetailView}>
-        </Route>
-        
-        <Route path={"/:featureId"} component={FeatureDetailView}/>
+        <Header />
+        <Route path={"/"} exact component={ProjectOverView}></Route>
+        <Route path={"/:_id"} exact component={ProjectDetailView}></Route>
+        <Route
+          path={"/api/features/:_id/:featureId"}
+          exact
+          component={FeatureDetailView}
+        ></Route>
       </Router>
     );
-  } 
   }
-  
-  export default App;
+}
+
+export default App;
