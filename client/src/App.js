86e1c532e99cf02ec0a214a97bc65c8e9bc8d026
Sebastian-Zoske <Sebastian.Zoske@student.htw-berlin.de>
2019-12-24 17:59:55 +0100

    fixed Router for da Footer

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,31 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import FAQ from "./components/FAQ";
 import ProjectOverView from "./components/ProjectOverView";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import CommentReview from './components/CommentReview';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CommentReview from "./components/CommentReview";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Header />
-        <Route path={"/"} exact component={ProjectOverView}></Route>
-        <Route path={"/:_id"} exact component={ProjectDetailView}></Route>
-        <Route path={"/commentreview"} component={CommentReview}></Route>
-        <Route path={"/:_id/:featureId"} exact component={FeatureDetailView}></Route>
-        <Route path={"/faq"} component={FAQ}></Route>
+
+        <Switch>
+          <Route
+            exact
+            path={"/"}
+            render={props => <ProjectOverView {...props} />}
+          />
+          <Route path={"/faq"} component={FAQ} />
+          <Route path={"/commentreview"} component={CommentReview} />
+          <Route
+            exact
+            path={"/:projectId"}
+            render={props => <ProjectDetailView {...props} />}
+          />
+          <Route path={"/:projectId/:featureId"} component={FeatureDetailView} />
+                
+        </Switch>
         <Footer />
       </Router>
     );
--- client/src/components/ProjectDetailView.jsx
+++ client/src/components/ProjectDetailView.jsx
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
 import FeaturePDV from "./FeaturePDV";
 import axios from "axios";
-import { BrowserRouter as Router, Link } from "react-router-dom";
 import config from '../config';
 
 class ProjectDetailView extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       features: [],
       name: "",
       comments: "",
-      projectId: ""
+      projectId: this.props.match.params.projectId
     };
   }
+  
+  componentDidMount() {
+    axios.get(
+      config.url + `/api/projects/` + this.state.projectId
+    )
+    .then(response => {
+      this.setState({ 
+        features: response.data.features,
+        name: response.data.name,
+        projectId: response.data._id});
+    })
+  }
+
   render() {
     return (
       <div className="container">
@@ -30,18 +43,6 @@ class ProjectDetailView extends Component {
         ))}
       </div>
     );
-  }
-  async componentDidMount() {
-    const promise = await axios.get(
-      config.url + `/api/projects/` + this.props.match.params._id
-    );
-    const features = promise.data.features;
-    const name = promise.data.name;
-    const _id = promise.data._id;
-    this.setState({ features });
-    this.setState({ name });
-    this.setState({ projectId: _id});
-    console.log(_id);
   }
 }
 export default ProjectDetailView;
