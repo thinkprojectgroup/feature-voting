2e955938bcf91f3b4a43a528469569bacb2240c8
Fellix Lausch <baumthefeljoy@live.de>
2019-12-31 16:01:59 +0100

    fix reading cookie mistake

--- server/middleware/userCookies.js
+++ server/middleware/userCookies.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 // Check if there is a valid userId cookie in the request, if not create a new user and set id as cookie
 module.exports = async (req, res, next) => {
-    var userId = req.userId;
+    var userId = req.cookies["userId"];
     if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
         // TODO: only create new user if cookie permission is given
         user = new User({})
