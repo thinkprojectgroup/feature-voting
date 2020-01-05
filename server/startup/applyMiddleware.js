c60323ccc05a3071ec3d5e1fa45c44e17b40fac6
Fellix Lausch <baumthefeljoy@live.de>
2020-01-05 14:11:30 +0100

    s

--- server/startup/applyMiddleware.js
+++ server/startup/applyMiddleware.js
@@ -11,7 +11,7 @@ const cors = require("cors")
 const path = require("path")
 
 module.exports = function (app) {
-  app.use(cors())
+  app.use(cors({credentials: true}))
 
   app.use(express.json({ limit: "10mb" }))
   app.use(express.urlencoded({ extended: true }))
