0fa6edc8a6f005c050113753e1309955ea411560
felixstahmer <felix.stahmer@t-online.de>
2020-01-08 22:33:33 +0100

    fix

--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://dfvdev:dynamicfeaturevoting2019@dynamicfeaturevoting-nzxlf.mongodb.net/test?retryWrites=true&w=majority',
-    client_id_1: '596132698210-554c0ihpr0kp9vg13v7irajr55v8m4eq.apps.googleusercontent.com', // Allowed ID from connecting Client
-}
