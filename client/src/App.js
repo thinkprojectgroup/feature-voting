12bfceaea047b94645f0919b633b4d23bc18d23d
Sebastian-Zoske <Sebastian.Zoske@student.htw-berlin.de>
2020-01-08 18:33:01 +0100

    Add Fingerprint hash, Set it to default header

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
-import "./App.css";
-import FeatureDetailView from "./components/Feature/FeatureDetailView";
-import ProjectDetailView from "./components/Project/ProjectDetailView";
-import Header from "./components/Header";
-import ProjectOverView from "./components/Project/ProjectOverView";
-import CommentReview from "./components/Comment/CommentReview";
-import SignIn from "./components/Auth/SignIn";
-import AdminOnly from "./components/Auth/AdminOnly";
-import AppWrapper from "./components/AppWrapper";
-import Footer from "./components/Footer";
-import FAQ from "./components/FAQ";
+import React, { Component } from 'react'
+import Fingerprint2 from 'fingerprintjs2'
+import axios from 'axios'
+import './App.css'
+import FeatureDetailView from './components/Feature/FeatureDetailView'
+import ProjectDetailView from './components/Project/ProjectDetailView'
+import Header from './components/Header'
+import ProjectOverView from './components/Project/ProjectOverView'
+import CommentReview from './components/Comment/CommentReview'
+import SignIn from './components/Auth/SignIn'
+import AppWrapper from './components/AppWrapper'
+import Footer from './components/Footer'
+import FAQ from './components/FAQ'
 import {
   BrowserRouter as Router,
   Route,
@@ -17,26 +18,44 @@ import {
   Redirect,
   useHistory,
   useLocation
-} from "react-router-dom";
-
-
-
+} from 'react-router-dom'
 
 class App extends Component {
-  state = {
-    role: "user",
-    isSignedIn: false,
-    idToken: null,
-    authIsLoaded: false
-  };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      role: 'user',
+      isSignedIn: false,
+      idToken: null,
+      authIsLoaded: false
+    }
+
+    this.createFingerPrint()
+  }
+  
+
+  createFingerPrint = () => {
+    setTimeout(function () {
+      Fingerprint2.get(function (components) {
+        // an array of: {key: ..., value: ...} pairs
+        // only keep the values
+        const values = components.map(component => component.value)
+        // hash the values
+        const hashedFingerprint = Fingerprint2.x64hash128(values.join(''), 31)
+        axios.defaults.headers.common['Hash'] = hashedFingerprint
+        console.log(hashedFingerprint) // <= send this
+      })
+    }, 500)
+  }
 
   // executed in <AppWrapper> after Google API and Auth is initalized
   // otherwise problems with async
   isReady = isReady => {
     this.setState({
       authIsLoaded: isReady
-    });
-  };
+    })
+  }
 
   // executed in <AppWrapper> and <SignIn>
   // ->  after User login and/or before App loads (check for signed in User)
@@ -45,10 +64,10 @@ class App extends Component {
       role: role,
       isSignedIn: isSignedIn,
       idToken: idToken
-    });
-  };
+    })
+  }
 
-  render() {
+  render () {
     return (
       <AppWrapper
         isReady={this.isReady}
@@ -60,42 +79,42 @@ class App extends Component {
               <Header role={this.state.role} />
               <Switch>
                 <Route
-                    exact
-                    path={"/"}
-                    render={props => 
-                      (this.state.role == 'admin') ? (
-                        <ProjectOverView {...props} />
-                      ) : (
-                        <Redirect to={"/login"} />
-                      )}
+                  exact
+                  path={'/'}
+                  render={props =>
+                    this.state.role == 'admin' ? (
+                      <ProjectOverView {...props} />
+                    ) : (
+                      <Redirect to={'/login'} />
+                    )
+                  }
                 />
                 <Route
                   exact
-                  path={"/commentreview"}
-                  render={props => 
-                    (this.state.role == 'admin') ? (
+                  path={'/commentreview'}
+                  render={props =>
+                    this.state.role == 'admin' ? (
                       <CommentReview {...props} />
                     ) : (
-                      <Redirect to={"/login"} />
-                    )}
+                      <Redirect to={'/login'} />
+                    )
+                  }
                 />
                 <Route
                   exact
-                  path={"/login"}
+                  path={'/login'}
                   render={props => (
                     <SignIn setAuthorisation={this.setAuthorisation} />
                   )}
                 />
-
-                <Route exact path={"/faq"} component={FAQ} />
-
+                <Route exact path={'/faq'} component={FAQ} />
                 <Route
                   exact
-                  path={"/:projectName"}
+                  path={'/:projectName'}
                   render={props => <ProjectDetailView {...props} />}
                 />
                 <Route
-                  path={"/:projectName/:featureId"}
+                  path={'/:projectName/:featureId'}
                   component={FeatureDetailView}
                 />
               </Switch>
@@ -104,8 +123,8 @@ class App extends Component {
           </div>
         )}
       </AppWrapper>
-    );
+    )
   }
 }
 
-export default App;
+export default App
