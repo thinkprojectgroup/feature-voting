7014e4a76abc0495b1b8ae8c46682ecc9ebf09c7
Felix Stahmer <40663095+felixstahmer@users.noreply.github.com>
2020-01-09 16:52:42 +0100

    Comment count in PDV (#213)
    * basic comment count functionality
    
    * wip
    
    * change to mongodb atlas, implement transactions for comments
    
    * change comment count in patch instead
    
    * commentCount displayed in PDV
    
    * fix
    
    * fix
    
    * fix
    
    * add env var for client_id
    
    * smol change
    
    Co-authored-by: BaumTheFeljoy <35038719+BaumTheFeljoy@users.noreply.github.com>

--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://dfvdev:dynamicfeaturevoting2019@ds241408.mlab.com:41408/feature-voting',
-    client_id_1: '596132698210-554c0ihpr0kp9vg13v7irajr55v8m4eq.apps.googleusercontent.com', // Allowed ID from connecting Client
-}
