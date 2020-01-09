6bedfcf73502bbb0328a9874309c9b64d2e69198
Fellix Lausch <baumthefeljoy@live.de>
2020-01-09 21:26:00 +0100

    wip, upload one image works, db not quite yet

--- client/src/components/FeatureForm.js
+++ client/src/components/FeatureForm.js
@@ -1,120 +1,128 @@
 import React, { Component } from "react";
 import axios from "axios";
 import FileBase from "react-file-base64"
+import { storage } from '../firebase-config';
 
 class FeatureForm extends Component {
-        state = {
-            headline: "",
-            description: "",
-            selectedFile: null,
-            loaded: 0,
-            imageData: String
+    state = {
+        headline: "",
+        description: "",
+        selectedFile: null,
+        loaded: 0,
+        firebaseImage: String,
+        currentImageName: String
+    };
+    checkMimeType = (files) => {
+        //getting file object
+        // let files = files
+        //define message container
+        let err = []
+        // list allow mime type
+        const types = ['image/png', 'image/jpeg']
+        // loop access array
+        for (var x = 0; x < files.length; x++) {
+            // compare file type find doesn't matach
+            if (types.every(type => files[x].type !== type)) {
+                // create error message and assign to container   
+                err[x] = files[x].type + ' is not a supported format\n';
+            }
         };
-        checkMimeType = (files) => {
-          //getting file object
-          // let files = files
-          //define message container
-          let err = []
-          // list allow mime type
-          const types = ['image/png', 'image/jpeg']
-          // loop access array
-          for (var x = 0; x < files.length; x++) {
-              // compare file type find doesn't matach
-              if (types.every(type => files[x].type !== type)) {
-                  // create error message and assign to container   
-                  err[x] = files[x].type + ' is not a supported format\n';
-              }
-          };
-          for (var z = 0; z < err.length; z++) {// if message not same old that mean has error 
-              // discard selected file
-              alert("Wrong Datatype!");
-              //event.target.value = null
-              return false;
-          }
-          return true;
-      }
-      maxSelectFile = (files) => {
-          // let files = files
-          if (files.length > 3) {
-              const msg = 'Only 3 images can be uploaded at a time'
-              // event.target.value = null
-              alert("Too many files!");
-              return false;
-          }
-          return true;
-      }
-      checkFileSize = (files) => {
-          // let files = files
-          let size = 2000000 //2MB
-          let err = [];
-          for (var x = 0; x < files.length; x++) {
-              // console.log(files[x].size)
-              if (files[x].size > size) {
-                  err[x] = files[x].type + 'is too large, please pick a smaller file\n';
-              }
-          };
-          for (var z = 0; z < err.length; z++) {// if message not same old that mean has error 
-              // discard selected file
-              alert("Your files are too big!");
-                //event.target.value = null
-          }
-          return true;
-      }
+        for (var z = 0; z < err.length; z++) {// if message not same old that mean has error 
+            // discard selected file
+            alert("Wrong Datatype!");
+            //event.target.value = null
+            return false;
+        }
+        return true;
+    }
+    maxSelectFile = (files) => {
+        // let files = files
+        if (files.length > 3) {
+            const msg = 'Only 3 images can be uploaded at a time'
+            // event.target.value = null
+            alert("Too many files!");
+            return false;
+        }
+        return true;
+    }
+    checkFileSize = (files) => {
+        // let files = files
+        let size = 2000000 //2MB
+        let err = [];
+        for (var x = 0; x < files.length; x++) {
+            // console.log(files[x].size)
+            if (files[x].size > size) {
+                err[x] = files[x].type + 'is too large, please pick a smaller file\n';
+            }
+        };
+        for (var z = 0; z < err.length; z++) {// if message not same old that mean has error 
+            // discard selected file
+            alert("Your files are too big!");
+            //event.target.value = null
+        }
+        return true;
+    }
 
-      onChange = (e) => {
+    onChange = (e) => {
         this.setState({ [e.target.name]: e.target.value });
         //console.log(this.state)
-        
-      }  
-
-
-      getBaseFile(files) {
-        // show the 1st image as example
-        var imageData = '';
-        if (this.maxSelectFile(files) && this.checkMimeType(files) && this.checkFileSize(files)){
-            imageData = files.map(file => file.base64.toString());
-
-            this.setState({
-              imageData: imageData
-          });
-
-          // TODO: FileBase Component needs to be cleared when check fails
-        }
-
-        // console.log(this.state);
 
     }
 
-      onSubmit = (e) => {
+
+    uploadImage(event) {
+        // show the 1st image as example
+        var imageData = ''
+        let currentImageName = "firebase-image-" + Date.now()
+
+        let uploadImage = storage.ref(`images/${currentImageName}`).put(event.target.files[0])
+
+        uploadImage.on('state_changed',
+            (snapshot) => { },
+            (error) => {
+                alert(error);
+            },
+            () => {
+                storage.ref('images').child(currentImageName).getDownloadURL().then(url => {
+                    this.setState({
+                        firebaseImage: url,
+                        currentImageName: currentImageName
+                    });
+                })
+            })
+    }
+
+    onSubmit = (e) => {
         e.preventDefault();
         //console.log(e);
-        //console.log(this.state);
+        console.log(this.state);
         //console.log(this.props.projectId);
 
-        const config = {     
-            headers: { 
-              'Content-Type': 'application/json' }
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
         }
 
         let data = JSON.stringify({
-          headline: this.state.headline,
-          description: this.state.description,
-          imageData: this.state.imageData
+            headline: this.state.headline,
+            description: this.state.description,
+            imageUrls: this.state.firebaseImage
         })
 
-        axios.post('/api/features/' + this.props.projectId , data, config)
-          .then((result) => {
-            console.log(result);
-          })
-          .catch(error => {
-            console.log(error);
-        });
-      }
+        axios.post('/api/features/' + this.props.projectName, data, config)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch(error => {
+                console.log(error.response);
+            });
+    }
 
-    render(){
-        const {headline, description } = this.state;
+    render() {
+        const { headline, description } = this.state;
 
-        return(
+        return (
 
             <div className="feature-form-container row col-12">
                 <form onSubmit={this.onSubmit} className="feature-form">
@@ -122,12 +130,12 @@ class FeatureForm extends Component {
                         Title:
                     </label>
                     <input type="text"
-                           name="headline"
-                           id="headline"
-                           className="headline col-12"
-                           value={headline}
-                           onChange={this.onChange}
-                           required
+                        name="headline"
+                        id="headline"
+                        className="headline col-12"
+                        value={headline}
+                        onChange={this.onChange}
+                        required
                     />
 
                     <label>
@@ -143,7 +151,7 @@ class FeatureForm extends Component {
                     />
 
                     <label>Upload Your Images </label>
-                    <FileBase type="file" multiple={true} onDone={this.getBaseFile.bind(this)} />
+                    <input type="file" className="process__upload-btn" onChange={(event) => this.uploadImage(event)} />
 
                     <button className="submit col-2" type="submit" value="Submit">Submit</button>
                 </form>
