ac2ebac610bd49b78bf58e00046e08288c49a8a1
Tanja Rosner <tr@pragma-shift.net>
2020-01-10 13:25:50 +0100

    added global comment review Button and login/logout Button Design

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,68 @@
 import React, { Component } from "react";
 import {withRouter, Link } from "react-router-dom";
+import axios from "axios";
+import config from "../config";
 
 class Header extends Component {
+
+
   
   render() {
 
-    // TODO: Add Real User Role Value
-    const isAdmin = this.props.role === 'admin' ? true : false;
 
+    //Logo Image on the left side
     const image = require("./img/logo.png");
 
+
+    //check if user is Admin
+    const isAdmin = this.props.role === 'admin' ? true : false;
+
+
+    //* LOGIN /LOGOUT BUTTON*//
+
+    // TODO: Add Real Login/Logout Logic
+    const loggedIn = false;
+
+    //initializing LOGIN/LOGOUT Buttons
+    let loginButton = "";
+    if(loggedIn){
+      loginButton = (
+          //TODO: Add Functionality
+          <button className="logout-button fas fa-sign-out-alt"></button>
+      );
+    } else if(!loggedIn){
+      loginButton = (
+          //TODO: Add Functionality
+          <button className="login-button fas fa-sign-in-alt" ></button>
+      );
+    }
+
+
+    //* COMMENT REVIEW BUTTON*//
+
+    //initializing the Comment Review Button and only show it for Admins
+    let commentReviewButton = "";
+    if(isAdmin){
+      commentReviewButton = (
+          <Link to={"/commentreview"}>
+            <div className="comment-review-button fas fa-comments"></div>
+          </Link>
+      );
+    }
+
+
+    //* BACK BUTTON*//
+
+    //Different root depths for Admins/Employees and regular users
     var rootDepth = isAdmin ? 1 : 2;
+    //Get the current pathname
     const pathname = this.props.location.pathname;
+    //Split the Path by "/"
     const pathnameArray = this.props.location.pathname.split('/');
 
+    //initialize backroute
     var backroute = "";
+    //backroute is current path without the last part
     for (let i = 1; i < pathnameArray.length - 1 ; i++) {
       backroute += "/";
       backroute += pathnameArray[i];
@@ -25,15 +73,18 @@ class Header extends Component {
       rootPath += pathnameArray[i];
     }
 
-    let backButton = (
-      <Link to={backroute}>
+    let backButton = "";
+    //check if backroute is deeper than rootpath, if then don't show backbutton
+    if(!(pathname === rootPath)){
+      backButton = (
+        <Link to={backroute}>
         <div className="back-button fas fa-arrow-left"></div>
       </Link>
     );
-
-    if(pathname === rootPath){
-      backButton = "";
     }
+
+
+
 
     return (
         <div className="row col-12 header">
@@ -43,8 +94,9 @@ class Header extends Component {
                 <img alt="" src={image} />
               </div>
             </Link>
-
             {backButton}
+            {loginButton}
+            {commentReviewButton}
           </div>
         </div>
     );
