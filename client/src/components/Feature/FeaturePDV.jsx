a9b7ef323d07c5d50312ef453a608dacdafba0be
taylinr <47357008+taylinr@users.noreply.github.com>
2020-01-15 10:37:40 +0100

    read-more link in PDV when feature-description has more than 150 char (#222)
    * added read-more link when feature-description has more than 50 characters
    
    * deleted unused css files and increased the character limit to 150
    
    * when you click on the read more button the more text appears
    
    * adjust npm stuff
    
    Co-authored-by: BaumTheFeljoy <35038719+BaumTheFeljoy@users.noreply.github.com>

--- client/src/components/Feature/FeaturePDV.jsx
+++ client/src/components/Feature/FeaturePDV.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 import axios from "axios";
 import config from '../../config';
 //import beispiel from "./img/computer.png";
-//import ReadMoreAndLess from "react-read-more-less";
+import ReadMoreAndLess from 'react-read-more-less';
 
 class FeaturePDV extends Component {
   constructor(props){
@@ -61,10 +61,15 @@ class FeaturePDV extends Component {
 
 
 
+
+
+
   render() {
     // TODO: Add real imagadata later
     var image = require("../img/computer.png");
    //  console.log(this.props.projectId);
+
+    let description = this.state.description;
 
     return (
       <div className="row feature-list-item">
@@ -99,20 +104,32 @@ class FeaturePDV extends Component {
 
 
         </div>
-        <Link to={"/" + this.state.projectName.split(" ").join("-") + "/" + this.state.featureId}>
-          <div className="col-8 feature-text">
-            <div className="title">
 
-                <h3>{this.state.title}</h3>{" "}
-            </div>
+          <div className="col-8 feature-text">
+            <Link to={"/" + this.state.projectName.split(" ").join("-") + "/" + this.state.featureId}>
+              <div className="title">
+
+                  <h3>{this.state.title}</h3>{" "}
+              </div>
+            </Link>
             <div className="description">
-              <p>{this.state.description}</p>
+
+              <ReadMoreAndLess
+                  ref={this.ReadMore}
+                  className="read-more-content"
+                  charLimit={151}
+                  readMoreText="Read more"
+                  readLessText="Read less"
+              >
+                {description}
+              </ReadMoreAndLess>
             </div>
+
             <div className="comment-count">
               <p>{this.state.commentCount} comments</p>
             </div>
           </div>
-
+        <Link to={"/" + this.state.projectName.split(" ").join("-") + "/" + this.state.featureId}>
           <div
             className="col-3 feature-image"
             style={{backgroundImage: "url(" + image + ")"}} >
@@ -122,6 +139,7 @@ class FeaturePDV extends Component {
       </div>
     );
   }
+
 }
 
 export default FeaturePDV;
