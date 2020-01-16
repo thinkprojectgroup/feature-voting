b01d4b72ed76dc43f74017b3dc494bc682ca3eb7
felixstahmer <felix.stahmer@t-online.de>
2020-01-16 19:37:45 +0100

    filter by project and links to project and feature for every comment in CommentReview page

--- client/src/components/Comment/CommentReview.jsx
+++ client/src/components/Comment/CommentReview.jsx
@@ -10,6 +10,11 @@ class CommentReview extends Component{
 
     this.state = {
         comments: [],
+        projectNames: [],
+        filteredProjectNames: [],
+        featureNames: [],
+        filteredFeatureNames: [],
+        outputComments: []
     };
     }
 
@@ -17,29 +22,84 @@ class CommentReview extends Component{
     axios.get(config.url + "/api/comments/")
     .then(res => {
         const comments = res.data; 
-        this.setState({comments: comments});
-        console.log(this.state)
+        this.setState({
+            comments: comments,
+            outputComments: comments
+        });
+        this.state.comments.map(comment => {
+            this.setState({
+                projectNames: this.state.projectNames.concat(comment.projectName),
+                featureNames: this.state.featureNames.concat(comment.featureName)
+            })
+        })
+        this.setState({
+            filteredProjectNames: this.removewithfilter(this.state.projectNames),
+            filteredFeatureNames: this.removewithfilter(this.state.featureNames)
+        })
     })
     .catch(error =>{
         console.log(error)
     });
+    }
 
+    removewithfilter = (arr) => { 
+        let outputArray = arr.filter(function(v, i, self) 
+        { 
+            // It returns the index of the first 
+            // instance of each value 
+            return i == self.indexOf(v); 
+        }); 
+          
+        return outputArray; 
+    }
+
+    handleProject = (e) => {
+        e.preventDefault()
+        let selectedName = e.target.value
+        let selectedComments = this.state.comments.filter(comment => {
+            return comment.projectName === selectedName
+        })
+
+        // console.log(selectedComments)
+
+        this.setState({
+            outputComments: []
+        }, () => this.setState({ outputComments: selectedComments }))
     }
 
 
+
     render(){
-        console.log(this.state.comments);
+        // console.log(this.state.outputComments)
         return(
+            <div>
+                <form>
+                    <select onChange={this.handleProject.bind(this)}>
+                        <option>
+                            Filter by Project
+                        </option>
+                        {this.state.filteredProjectNames.map(name => (
+                            <option value={name}> 
+                            {name}
+                            </option>
+                        ))}
+                    </select>
+                </form>
                 <div className="comment-section container row">
-                    {this.state.comments.map(comment => (
-                    <CommentCR 
+                    {this.state.outputComments.map(comment => {
+                    return ( <CommentCR 
                         commentId={comment._id}
                         name={comment.name}
                         content={comment.content}
                         date={comment.dateCreated}
+                        featureName={comment.featureName}
+                        projectName={comment.projectName}
+                        featureId={comment.featureId}
                     />
-                    ))} 
+                    )
+                    })} 
                 </div>                     
+            </div>
         );
     }
 }
