eddf7db119eb0261c2abe6727e1874cac58dfdba
Sebastian-Zoske <s0554384@htw-berlin.de>
2020-01-18 01:06:02 +0100

    Auth Middleware - Redirect After Login (#233)
    * authorisation middleware - login redirect
    
    * formatting
    
    * Update ProjectOverView.jsx
    
    * Update ProjectOverView.jsx
    
    * Update App.js
    
    * Update checkAuth.js
    
    * Update checkAuth.js
    
    * remove Create User in checkAuth
    
    * Update CommentCR.js
    
    * fix render issue with login-redirect - installed react-google-login
    
    * Update AuthService.js

--- client/src/components/Project/ProjectDetailView.jsx
+++ client/src/components/Project/ProjectDetailView.jsx
@@ -5,7 +5,6 @@ import axios from "axios";
 import config from '../../config';
 import { Button } from "reactstrap";
 
-
 class ProjectDetailView extends Component {
   constructor(props) {
     super(props);
