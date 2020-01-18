485b824d6ee59d69d5824870f30ef680fdbae90b
taylinr <47357008+taylinr@users.noreply.github.com>
2020-01-18 13:43:09 +0100

    added admin rights page (#263)

---
+++ client/src/components/User/User.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class User extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: this.props.email,
+            newRole: this.props.newRole,
+            clicked: false
+        };
+
+        this.handlePromote = this.handlePromote.bind(this);
+    }
+    handlePromote = () => {
+        var self = this;
+        const role = this.state.newRole;
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+
+        this.props.reRender(this.state.email, this.state.newRole);
+
+        let data = JSON.stringify({
+            role: role
+        });
+        axios
+            .post("/api/users/" + this.state.email, data, config)
+            .then(function(response) {
+                console.log(response);
+                self.setState({
+                    clicked: true
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    };
+
+    render() {
+        return (
+            <div>
+                {!this.state.clicked ? (
+                    <div>
+                        {this.props.newRole == "admin" ? (
+                            <div className="row">
+                                <div className="col-1">
+                                    <button className="accept" onClick={() => this.handlePromote()}><i className="fas fa-plus"></i></button>
+                                </div>
+                                <div className="col-11">
+                                    <p>{this.state.email}</p>{" "}
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="row">
+                                <div className="col-1">
+                                    <button className="decline" onClick={() => this.handlePromote()}><i className="fas fa-times"></i></button>
+                                </div>
+                                <div className="col-11">
+                                    <p>{this.state.email}</p>{" "}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                ) : null}
+            </div>
+        );
+    }
+}
+
+export default User;
