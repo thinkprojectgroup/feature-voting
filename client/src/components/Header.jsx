7addb53a317e04efaf58a52f22659b941a56d332
Sebastian-Zoske <s0554384@htw-berlin.de>
2020-01-18 18:15:40 +0100

    Header Buttons - Commentreview & Login/Logout (#270)
    * added global comment review Button and login/logout Button Design
    
    * login blue, logout grey
    
    * comment review button only on POV
    
    * Buttons in Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,68 @@
 import React, { Component } from "react";
 import {withRouter, Link } from "react-router-dom";
+import axios from "axios";
+import config from "../config";
 
 class Header extends Component {
+
+  redirectToLogin = () => {
+    this.props.history.push("/login");
+  }
+
+  logout = () => {
+    var auth2 = window.gapi.auth2.getAuthInstance()
+    auth2.signOut().then(() => {
+      this.props.setAuthorisation(null, false, null)
+      auth2.disconnect()
+      this.props.history.push('/login')
+    })
+  }
   
   render() {
 
-    // TODO: Add Real User Role Value
-    const isAdmin = this.props.role === 'admin' ? true : false;
-
+    //Logo Image on the left side
     const image = require("./img/logo.png");
 
+
+    //check if user is Admin
+    const isAdmin = this.props.role === 'admin' ? true : false;
+
+
+    //* LOGIN /LOGOUT BUTTON*//
+
+    // TODO: Add Real Login/Logout Logic
+    const loggedIn = this.props.isSignedIn;
+
+    //initializing LOGIN/LOGOUT Buttons
+    let loginButton = "";
+    if (loggedIn) {
+      loginButton = (
+          //TODO: Add Functionality
+          <button className="logout-button fas fa-sign-out-alt" onClick={this.logout}></button>
+      );
+    } else if(!loggedIn){
+      loginButton = (
+          //TODO: Add Functionality
+          <button className="login-button fas fa-sign-in-alt" onClick={this.redirectToLogin}></button>
+      );
+    }
+
+
+
+
+
+    //* BACK BUTTON*//
+
+    //Different root depths for Admins/Employees and regular users
     var rootDepth = isAdmin ? 1 : 2;
+    //Get the current pathname
     const pathname = this.props.location.pathname;
+    //Split the Path by "/"
     const pathnameArray = this.props.location.pathname.split('/');
 
+    //initialize backroute
     var backroute = "";
+    //backroute is current path without the last part
     for (let i = 1; i < pathnameArray.length - 1 ; i++) {
       backroute += "/";
       backroute += pathnameArray[i];
@@ -25,14 +73,27 @@ class Header extends Component {
       rootPath += pathnameArray[i];
     }
 
-    let backButton = (
-      <Link to={backroute}>
-        <div className="back-button fas fa-arrow-left"></div>
-      </Link>
-    );
+    let backButton = "";
+    //check if backroute is deeper than rootpath, if then don't show backbutton
+    if(!(pathname === rootPath)){
+      backButton = (
+          <Link to={backroute}>
+            <button className="back-button fas fa-arrow-left"></button>
+          </Link>
+      );
+    }
 
-    if(pathname === rootPath){
-      backButton = "";
+
+    //* COMMENT REVIEW BUTTON*//
+
+    //initializing the Comment Review Button and only show it for Admins
+    let commentReviewButton = "";
+    if(isAdmin && pathname == "/") {
+      commentReviewButton = (
+          <Link to={"/commentreview"}>
+            <button className="comment-review-button fas fa-comments" title="Link to Comment Review Page"></button>
+          </Link>
+      );
     }
 
     return (
@@ -43,8 +104,9 @@ class Header extends Component {
                 <img alt="" src={image} />
               </div>
             </Link>
-
             {backButton}
+            { this.props.location.pathname !== '/login' && loginButton}
+            {commentReviewButton}
           </div>
         </div>
     );
