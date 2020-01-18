5d4f3adec0edb8bb78dacee3f611957c5a5eaab5
taylinr <47357008+taylinr@users.noreply.github.com>
2020-01-18 14:07:49 +0100

    added delete and form to POV (#264)

--- client/src/components/Project/ProjectOverView.jsx
+++ client/src/components/Project/ProjectOverView.jsx
@@ -1,15 +1,26 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import config from '../../config'
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import config from "../../config";
+import ProjectForm from "./ProjectForm";
 
 class ProjectOverView extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      projects: []
-    }
+      projects: [],
+      idToken: this.props.idToken,
+      show: false
+    };
+    this.toggleShow = this.toggleShow.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+
   }
+  toggleShow = () => {
+    this.setState({ show: !this.state.show });
+    document.getElementById("form-button").classList.toggle("cross");
+    // console.log(this.state.show);
+  };
 
   componentDidMount () {
     axios
@@ -24,16 +35,53 @@ class ProjectOverView extends Component {
       })
   }
 
+  handleDelete = id => {
+    var self = this;
+    var newState = self.state.projects.filter(project => project._id != id);
+    self.setState({
+      projects: newState
+    });
+    // console.log(comment._id)
+    axios
+        .delete("/api/projects/" + id)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+  };
+
+
+
+
   render () {
     return (
-      <div className='container'>
-        <h1>Projects</h1>
-        {this.state.projects.map(project => (
-          <Link to={'/' + project.name.toString().split(' ').join('-')}>
-            <div className='row project-list-item'>
-              <h3>{project.name}</h3>{' '}
+        <div className="container row">
+          <div className="row">
+            <div className="col-11 project-name">
+              <h1>Projects</h1>
             </div>
-          </Link>
+            <div className="col-1 add-button">
+              <button onClick={this.toggleShow} className="add" id="form-button">
+                <i className="fas fa-plus"></i>
+              </button>
+            </div>
+          </div>
+          {this.state.show ? <ProjectForm /> : null}
+        {this.state.projects.map(project => (
+            <div>
+              <Link to={"/" + project.name.toString().split(" ").join("-")}>
+                <div className="row project-list-item col-11">
+                  <h3>{project.name}</h3>{" "}
+                </div>
+              </Link>
+              <div className="col-1 delete-project">
+                <button onClick={() => this.handleDelete(project._id)} className="decline" >
+                  <i className="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
         ))}
       </div>
     )
