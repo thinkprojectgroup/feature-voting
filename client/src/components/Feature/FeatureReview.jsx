4b2eb5d8c947bd111a63e97247fa318ec8a9623d
Tanja Rosner <tr@pragma-shift.net>
2020-01-27 18:52:00 +0100

    feature review conditional rendering wieder raus

--- client/src/components/Feature/FeatureReview.jsx
+++ client/src/components/Feature/FeatureReview.jsx
@@ -30,7 +30,7 @@ class FeatureReview extends Component {
                             />
                         ) : null
                     )}
-                </div>: null}
+                </div>
             </div>
         );
     }
