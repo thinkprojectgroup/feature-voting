db8d2e08a17efc149c92bdecec3fb2d54260a772
Tanja Rosner <tr@pragma-shift.net>
2020-01-27 12:22:51 +0100

    in user settings werden namen statt mailadressen angezeigt

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -41,6 +41,20 @@ class User extends Component {
     };
 
     render() {
+
+
+        let loginName="";
+
+        if(this.state.email){
+            var loginNameArray = (this.state.email.substring(0, this.state.email.indexOf("@"))).split(".");
+
+            for (var i = 0; i < loginNameArray.length; i++) {
+                loginName += loginNameArray[i].charAt(0).toUpperCase() + loginNameArray[i].slice(1) + " ";
+            }
+        }
+
+
+
         return (
             <div className="user-item row">
                 {!this.state.clicked ? (
@@ -51,7 +65,7 @@ class User extends Component {
                                     <button className="accept" onClick={() => this.handlePromote()} title="Promote to admin"><i className="fas fa-plus"></i></button>
                                 </div>
                                 <div className="col-11 user-name">
-                                    <p>{this.state.email}</p>{" "}
+                                    <p>{loginName}</p>{" "}
                                 </div>
                             </div>
                         ) : (
@@ -60,7 +74,7 @@ class User extends Component {
                                     <button className="decline" onClick={() => this.handlePromote()} title="Downgrade to regular user"><i className="fas fa-times"></i></button>
                                 </div>
                                 <div className="col-11 user-name">
-                                    <p>{this.state.email}</p>{" "}
+                                    <p>{loginName}</p>{" "}
                                 </div>
                             </div>
                         )}
